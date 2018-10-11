5f9c8ef6646a3fe94a0e688099b0848d0e7ef5a1
igorbrasileiro <brasileiro456@gmail.com>
2018-10-10 22:24:08 -0300

    adding prettier & redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { register } from './serviceWorker';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
+
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger'); // eslint-disable-line
+  middlewares.push(logger);
+}
+
+middlewares.push(thunk);
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+register();
