33d3dd15590042bf1ef28adb3b8d25376253b58b
igorbrasileiro <brasileiro456@gmail.com>
2018-10-31 23:34:25 -0300

    Refactor SubjectTable

--- src/components/UserInfo/SubjectTable.js
+++ src/components/UserInfo/SubjectTable.js
@@ -11,44 +11,40 @@ import {
   withStyles,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 const styles = theme => ({
   title: {
     paddingTop: theme.spacing.unit * 3,
     margin: theme.spacing.unit * 3,
   },
+  paper: {
+    width: '70%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
 });
 
-const Wrapper = styled.div`
-  width: 70%;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
 const SubjectTable = ({ classes }) => (
-  <Wrapper>
-    <Paper>
-      <Typography variant="title" className={classes.title}>
-        Lista de Disciplinas
-      </Typography>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell variant="head">Disciplina</TableCell>
-            <TableCell variant="head">Sheetid</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Portugues</TableCell>
-            <TableCell>saudad-asdadsadsuda-</TableCell>
-          </TableRow>
-        </TableBody>
-        <TableFooter>é Nós</TableFooter>
-      </Table>
-    </Paper>
-  </Wrapper>
+  <Paper className={classes.paper}>
+    <Typography variant="title" className={classes.title}>
+      Lista de Disciplinas
+    </Typography>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell variant="head">Disciplina</TableCell>
+          <TableCell variant="head">Sheetid</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell>Portugues</TableCell>
+          <TableCell>saudad-asdadsadsuda-</TableCell>
+        </TableRow>
+      </TableBody>
+      <TableFooter>é Nós</TableFooter>
+    </Table>
+  </Paper>
 );
 
 SubjectTable.propTypes = {
