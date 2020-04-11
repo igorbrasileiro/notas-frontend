935927f1a68580b7ff5bf62c3367d5849d670c21
igorbrasileiro <brasileiro456@gmail.com>
2020-04-11 13:51:48 -0300

    Add graphql

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
 import theme from "../utils/theme";
 import Route from "./Routes";
 
+const Client = new ApolloClient({
+  uri: "http://localhost:8001/",
+  headers: {
+    Authorization: localStorage.getItem("token"),
+  },
+});
+
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <Route />
-    </MuiThemeProvider>
+    <ApolloProvider client={Client}>
+      <MuiThemeProvider theme={theme}>
+        <Route />
+      </MuiThemeProvider>
+    </ApolloProvider>
   );
 };
 
